c5f9d9e6d6e9c11557067a1bd20944fcdbf10d67
ardi <ardisaint20@gmail.com>
2026-08-06 23:46:48 +0700

    Update sitemap domain to mibucoin.com

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mibu-token.vercel.app',
+      url: 'https://mibucoin.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
